e0c2c16e61be820f4a885dd68089f66e4ad7a980
Sunny Freshheads <s-feijen@hotmail.nl>
2023-10-16 13:46:56 +0200

    fix formatting

--- nextjs-pages/src/pages/api/exit-preview.ts
+++ nextjs-pages/src/pages/api/exit-preview.ts
@@ -1,9 +1,9 @@
 // ./nextjs-pages/src/pages/api/exit-preview.ts
 
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default function exit(req: NextApiRequest, res: NextApiResponse) {
-  res.setDraftMode({ enable: false });
-  res.writeHead(307, { Location: '/' });
-  res.end();
+    res.setDraftMode({ enable: false })
+    res.writeHead(307, { Location: '/' })
+    res.end()
 }
