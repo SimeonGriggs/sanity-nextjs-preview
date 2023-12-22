ad4a0dc98a29a3cc0d76890b6ba1ff92f7bca90c
Simeon Griggs <simeon@hey.com>
2023-12-22 13:32:37 +0000

    remove old 'pages' example

--- nextjs-pages/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {domains: ['cdn.sanity.io']}
-}
-
-module.exports = nextConfig
--- nextjs-pages/src/pages/api/preview.ts
+++
@@ -1,9 +0,0 @@
-// ./nextjs-pages/src/pages/api/preview.ts
-
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default function preview(req: NextApiRequest, res: NextApiResponse) {
-    res.setDraftMode({ enable: true })
-    res.writeHead(307, { Location: '/' })
-    res.end()
-}
